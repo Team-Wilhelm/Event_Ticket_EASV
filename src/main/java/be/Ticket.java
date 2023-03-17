99765da308e7d8ef82bc6f6804972e351cff8d88
mariaruth1 <113031776+mariaruth1@users.noreply.github.com>
2023-03-17 11:47:16 +0100

    ticket BE

--- src/main/java/be/Ticket.java
+++ src/main/java/be/Ticket.java
@@ -1,6 +1,58 @@
 package be;
 
+import java.util.UUID;
+
 public class Ticket {
     private Event event;
+    private Customer customer;
+    private UUID id;
+    private String ticketType, ticketQR;
 
+    public Ticket(Event event, Customer customer, String ticketType, String ticketQR) {
+        this.event = event;
+        this.customer = customer;
+        this.ticketType = ticketType;
+        this.ticketQR = ticketQR;
+    }
+
+    public Ticket(Event event, Customer customer, String ticketType, String ticketQR, UUID id) {
+        this(event, customer, ticketType, ticketQR);
+        this.id = id;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getTicketType() {
+        return ticketType;
+    }
+
+    public void setTicketType(String ticketType) {
+        this.ticketType = ticketType;
+    }
+
+    public String getTicketQR() {
+        return ticketQR;
+    }
+
+    public void setTicketQR(String ticketQR) {
+        this.ticketQR = ticketQR;
+    }
+
+    public UUID getId() {
+        return id;
+    }
 }
